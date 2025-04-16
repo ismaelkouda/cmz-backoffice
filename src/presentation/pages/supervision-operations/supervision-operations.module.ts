42e1d18b8ede62e1994f3f1ccb66994aca3ff014
ismaelkouda <ismaelkouda00@gmail.com>
2025-04-16 17:06:46 +0000

    Fix: conflits in supervisor system UI

--- src/presentation/pages/supervision-operations/supervision-operations.module.ts
+++ src/presentation/pages/supervision-operations/supervision-operations.module.ts
@@ -14,29 +14,8 @@ import { DemandeShowComponent } from './feature/demande-show/demande-show.compon
 import { NgModule } from '@angular/core';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-<<<<<<< HEAD
 import { AngularMultiSelectModule } from 'angular2-multiselect-dropdown';
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
-=======
-import { EditableRow, TableModule } from 'primeng/table';
-import { InputTextModule } from 'primeng/inputtext';
-import { DialogModule } from 'primeng/dialog';
-import { ToolbarModule } from 'primeng/toolbar';
-import { TooltipModule } from 'primeng/tooltip';
-import { ButtonModule } from 'primeng/button';
-import { ConfirmDialogModule } from 'primeng/confirmdialog';
-import { CalendarModule } from 'primeng/calendar';
-import { MultiSelectModule } from 'primeng/multiselect';
-import { FieldsetModule } from 'primeng/fieldset';
-import { TabViewModule } from 'primeng/tabview';
-import { DropdownModule } from 'primeng/dropdown';
-import { InputTextareaModule } from 'primeng/inputtextarea';
-import { CheckboxModule } from 'primeng/checkbox';
-import { BadgeModule } from 'primeng/badge';
-import { InputSwitchModule } from 'primeng/inputswitch';
-import { RadioButtonModule } from 'primeng/radiobutton';
-import { DynamicDialogModule } from 'primeng/dynamicdialog';
->>>>>>> origin/backup_v15
 import { SupervisionOperationsRoutingModule } from './supervision-operations-routing.module';
 import { SupervisionOperationService } from './data-access/supervision-operation.service';
 import { SharedModule } from 'src/shared/shared.module';
@@ -66,34 +45,11 @@ import { ClaimsApiService } from '../overseeing-operations/data-access/claims/se
 @NgModule({
     imports: [
         SharedModule,
-<<<<<<< HEAD
         SupervisionOperationsRoutingModule,
         NgSelectModule,
         NgbModule,
         AngularMultiSelectModule,
         Ng2SearchPipeModule,
-=======
-        InputTextModule,
-        ToolbarModule,
-        ButtonModule,
-        ConfirmDialogModule,
-        DialogModule,
-        CalendarModule,
-        FieldsetModule,
-        TabViewModule,
-        MultiSelectModule,
-        InputTextareaModule,
-        TabViewModule,
-        DropdownModule,
-        SplitButtonModule,
-        TooltipModule,
-        CheckboxModule,
-        BadgeModule,
-        InputSwitchModule,
-        RadioButtonModule,
-        DynamicDialogModule,
-        PaginatorModule,
->>>>>>> origin/backup_v15
         NgxPaginationModule,
     ],
     declarations: [
--- src/presentation/pages/supervision-systeme/supervision-systeme.module.ts
+++ src/presentation/pages/supervision-systeme/supervision-systeme.module.ts
@@ -5,28 +5,7 @@ import { NgSelectModule } from '@ng-select/ng-select';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { NgxPaginationModule } from 'ngx-pagination';
-<<<<<<< HEAD
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
-=======
-import { TableModule } from 'primeng/table';
-import { InputTextModule } from 'primeng/inputtext';
-import { DialogModule } from 'primeng/dialog';
-import { ToolbarModule } from 'primeng/toolbar';
-import { TooltipModule } from 'primeng/tooltip';
-import { ButtonModule } from 'primeng/button';
-import { ConfirmDialogModule } from 'primeng/confirmdialog';
-import { CalendarModule } from 'primeng/calendar';
-import { MultiSelectModule } from 'primeng/multiselect';
-import { FieldsetModule } from 'primeng/fieldset';
-import { TabViewModule } from 'primeng/tabview';
-import { DropdownModule } from 'primeng/dropdown';
-import { InputTextareaModule } from 'primeng/inputtextarea';
-import { CheckboxModule } from 'primeng/checkbox';
-import { BadgeModule } from 'primeng/badge';
-import { InputSwitchModule } from 'primeng/inputswitch';
-import { RadioButtonModule } from 'primeng/radiobutton';
-import { DynamicDialogModule } from 'primeng/dynamicdialog';
->>>>>>> origin/backup_v15
 import { EtatsServicesComponent } from './ui/etats-services/etats-services.component';
 import { TauxDesChargesComponent } from './ui/taux-des-charges/taux-des-charges.component';
 import { SharedModule } from 'src/shared/shared.module';
@@ -39,35 +18,12 @@ import { SharedModule } from 'src/shared/shared.module';
     ],
     imports: [
         SharedModule,
-<<<<<<< HEAD
         SupervisionSystemeRoutingModule,
         NgSelectModule,
         NgbModule,
         AngularMultiSelectModule,
         NgxPaginationModule,
         Ng2SearchPipeModule,
-=======
-        NgxPaginationModule,
-        InputTextModule,
-        ToolbarModule,
-        ButtonModule,
-        ConfirmDialogModule,
-        DialogModule,
-        CalendarModule,
-        FieldsetModule,
-        TabViewModule,
-        MultiSelectModule,
-        InputTextareaModule,
-        TabViewModule,
-        DropdownModule,
-        SplitButtonModule,
-        TooltipModule,
-        CheckboxModule,
-        BadgeModule,
-        InputSwitchModule,
-        RadioButtonModule,
-        DynamicDialogModule,
->>>>>>> origin/backup_v15
     ]
 })
 export class SupervisionSystemeModule { }
