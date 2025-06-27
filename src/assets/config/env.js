fda47a71ed6c977afb6649dfb05d4c253c887396
ismaelkouda <ismaelkouda00@gmail.com>
2025-06-27 19:23:54 +0000

    fix issues in all features

--- src/assets/config/env.js
+++ src/assets/config/env.js
@@ -1,21 +1,20 @@
-
-        (function (window) {
-            window.__env = {
-    "verifyIdentityDocumentUrl": "https://sim-monitoring.cateli.io:8013/",
-    apiUrl: "http://10.10.70.64:8001/api/v1/",
-        fileUrl: "http://10.10.70.64:8001/",
-    "environmentDeployment": "DEV",
-    "enableDebug": true,
-    "headerSettings": {
-        "appTypePS": "PATRIMOINE SIM",
-        "appTypeSM": "SIM MONITORING"
-    },
-    "messageApp": {
-        "sourceStockTenantSim": "Le système utilisera une SIM blanche du Stock du Tenant",
-        "sourceStockOrangeSim": "Orange fournira la SIM...",
-        "sourceSoldeDotation": "Le solde de la dotation Data...",
-        "sourceSoldeDotationOrange": "Orange fera le dépôt..."
-    }
-};
-        })(this);
-        
+(function (window) {
+    window.__env = {
+        verifyIdentityDocumentUrl: 'https://sim-monitoring.cateli.io:8013/',
+        environmentDeployment: 'DEV',
+        apiUrl: 'http://10.10.70.64:8001/api/v1/',
+        fileUrl: 'http://10.10.70.64:8001/',
+        enableDebug: true,
+        headerSettings: {
+            appTypePS: 'PATRIMOINE SIM',
+            appTypeSM: 'SIM MONITORING',
+        },
+        messageApp: {
+            sourceStockTenantSim:
+                'Le système utilisera une SIM blanche du Stock du Tenant',
+            sourceStockOrangeSim: 'Orange fournira la SIM...',
+            sourceSoldeDotation: 'Le solde de la dotation Data...',
+            sourceSoldeDotationOrange: 'Orange fera le dépôt...',
+        },
+    };
+})(this);
